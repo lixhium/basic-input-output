5da2677353e81bb5f3a81338cba262b0b7c7ffc5
lixhium <notkoishi@gmail.com>
2023-12-30 13:27:00 +0800

    cpu fan

--- main.c
+++ main.c
@@ -1,20 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 
-void change_x(int *x)
+#define MAX_STRING_SIZE 100
+#define BEST_GAME_IN_THE_WORLD "roblox"
+
+void input(char buffer[MAX_STRING_SIZE])
 {
-    *x = 0;
+    // warning, I don't like how it use MAX_STRING_SIZE instead of the actual string.
+    fgets(buffer, MAX_STRING_SIZE, stdin);
+    buffer[strlen(buffer) - 1] = '\0'; // remove the end of string new line to null terminator or end of line idk
+}
+
+void change_input_to_roblox(char buffer[MAX_STRING_SIZE])
+{
+    char roblox_string[] = "roblox";
+    strcpy(buffer, roblox_string);
 }
 
 int main()
 {
-    int x = 100;
+    char buffer[MAX_STRING_SIZE];
 
-    printf("%d\n", x);
+    printf("what is the best game in the world?: ");
+    input(buffer);
 
-    change_x(&x);
+    if(strcmp(buffer, BEST_GAME_IN_THE_WORLD) != 0)
+    {
+        change_input_to_roblox(buffer);
+    }
 
-    printf("%d\n", x);
-
+    printf("%s\n", buffer);
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 I'm learning c, shut up
+
+idk this "program" only purpose is to teach me c idk 
